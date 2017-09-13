c313a6a3e56833869215d07bca842434e9c0b7c0
Mattermack <charles.mack@mattersight.com>
2017-09-12 21:31:26 -0500

    Added examples of publishing and pulling from Pact Broker

---
+++ example-api/test/publish-pact.php
@@ -0,0 +1,16 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$uriOptions = new \PhpPact\PactUriOptions("http://ec2-34-212-75-79.us-west-2.compute.amazonaws.com" );
+$connector = new \PhpPact\PactBrokerConnector($uriOptions);
+
+$dir = "D:\\Temp\\pact-examples\\";
+$file = $dir . "exampleonemeetupapiclient-meetupapi.json";
+
+$statusCode = $connector->PublishFile($file, '1.0.1');
+
+$dir = "D:\\Temp\\pact-examples\\";
+$file = $dir . "exampletwomeetupapiclient-meetupapi.json";
+
+$statusCode = $connector->PublishFile($file, '3.1.7');
---
+++ example-api/test/pull-pact-via-broker.php
@@ -0,0 +1,15 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$uriOptions = new \PhpPact\PactUriOptions("http://ec2-34-212-75-79.us-west-2.compute.amazonaws.com" );
+$connector = new \PhpPact\PactBrokerConnector($uriOptions);
+
+// particular pact - latest version
+$pact = $connector->RetrievePact("MeetupAPI", "ExampleTwoMeetupApiClient");
+echo \json_encode($pact,JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+
+// get all pacts for this provider
+$pacts = $connector->RetrieveLatestProviderPacts("MeetupAPI");
+$pact = array_pop($pacts);
+echo \json_encode($pact,JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
